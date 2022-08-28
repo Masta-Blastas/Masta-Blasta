96b09f793f86b31bb961a6c59f513241758b1729
Gerald Clark <geraldclarkaudio@gmail.com>
2022-08-27 23:50:27 -0500

    basic melee enemy movement set up.

---
+++ Assets/Scripts/Enemy/EnemyBaseClass.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.AI;
+
+public abstract class EnemyBaseClass : MonoBehaviour
+{
+    //Making variables protected allows only classes that inherit from this class to manipulate them. 
+    [SerializeField]
+    protected float HP;
+    [SerializeField]
+    protected float speed;
+    [SerializeField]
+    protected float pointValue;
+
+    private Player player;
+
+    public ScoreSystem scoreSystem;
+
+    public UnityEvent death;
+
+    //waypoints 
+    [SerializeField]
+    protected List<Transform> wayPoints; // can change size of a list at run time if we need to. Cannot change an array size. 
+    [SerializeField]
+    protected int currentTarget;
+    protected bool reversing;
+    protected bool targetReached;
+
+    protected NavMeshAgent _agent;
+    [SerializeField]
+    protected Animator anim;
+    
+   
+    public virtual void Start()
+    {
+        player = GameObject.Find("XR Rig").GetComponent<Player>();
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    public virtual void Attack()
+    {
+        //do some damage
+        Debug.Log("My name is " + this.gameObject.name + " and I am attacking");
+    }
+
+    public virtual void Update()
+    {
+        Patroling();
+    }
+
+    public virtual void Patroling()
+    {
+        if (wayPoints.Count > 0) // are there waypoints?
+        {
+            if (wayPoints[currentTarget] != null)// does the current target exist?
+            {
+                _agent.SetDestination(wayPoints[currentTarget].position);
+                
+
+                float distance = Vector3.Distance(transform.position, wayPoints[currentTarget].position); // distance between target and enemy
+
+                if(distance < 1.0f)
+                {
+                    anim.SetBool("Walk", false);
+                }
+
+                else if(distance > 1)
+                {
+                    anim.SetBool("Walk", true);
+                }
+
+                if (distance < 1.0f && targetReached == false)
+                {
+                    targetReached = true;
+
+                    StartCoroutine(Idle());
+                }
+            }
+        }
+    }
+
+    IEnumerator Idle() // target reached is false, so resume the if statement in Patroling. 
+    {
+
+        yield return new WaitForSeconds(Random.Range(2.0f, 5.0f)); // pause for 2 - 5 seconds. 
+        
+        if(reversing == true)
+        {
+            currentTarget--;
+
+            if (currentTarget == 0) // there are no more waypoints to decrement. 
+            {
+                reversing = false;
+                currentTarget = 0; // set to zero
+            }
+        }
+
+        else if(reversing == false)
+        {
+            currentTarget++;
+
+            if (currentTarget == wayPoints.Count)  //if at the end of the waypoint list, reverse. 
+            {
+                //made it to the end. reverse
+                reversing = true;
+                currentTarget--;
+            }
+        }
+
+        targetReached = false;
+    }
+
+}
